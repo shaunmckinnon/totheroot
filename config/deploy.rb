54d92f451e2b8121719969a512e186a15e13957a
Shaun McKinnon <totheroot@hotmail.ca>
2016-03-22 10:56:50 -0400

    Added DEVISE secret key
    Signed-off-by: Shaun McKinnon <totheroot@hotmail.ca>

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rben
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
 
-set :linked_files, %w(config/database.yml config/secrets.yml)
+set :linked_files, %w(config/database.yml config/secrets.yml, config/initializers/devise.rb)
 
 ## Defaults:
 # set :scm,           :git
