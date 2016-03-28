ac155518fd4fa57ec4d85f25e78ee63c3db9d9d9
Shaun McKinnon <totheroot@hotmail.ca>
2016-03-28 18:24:47 -0400

    grrrrr
    Signed-off-by: Shaun McKinnon <totheroot@hotmail.ca>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,6 +7,10 @@ class HomeController < ApplicationController
         @etsy_products = EtsyProduct.search(params[:term])
         render json: @etsy_products, status: :ok, message: 'Success'
       end
+      format.js do
+        @etsy_products = EtsyProduct.search(params[:term])
+        render json: @etsy_products, status: :ok, message: 'Success'
+      end
     end
   end
 
