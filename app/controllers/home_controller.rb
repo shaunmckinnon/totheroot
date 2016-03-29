ed46ccdf1e580f3e3aaa274b4f8c5d2a9584e187
Shaun McKinnon <totheroot@hotmail.ca>
2016-03-28 21:00:45 -0400

    asdf
    Signed-off-by: Shaun McKinnon <totheroot@hotmail.ca>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
     respond_to do |format|
       format.json do
         @etsy_products = EtsyProduct.search(params[:term])
-        File.open "/tmp/test.json", "w" do |f|
+        File.open "test.json", "w" do |f|
           f.puts @etsy_products.to_json
         end
         render json: @etsy_products
