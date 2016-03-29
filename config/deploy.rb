eac08d48dca3329193926db26529831a4342642a
Shaun McKinnon <totheroot@hotmail.ca>
2016-03-28 22:08:27 -0400

    fixed
    Signed-off-by: Shaun McKinnon <totheroot@hotmail.ca>

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,6 +28,7 @@ set :rbenv_ruby, '2.2.4'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
+set :rails_env, "production"
 
 set :linked_files, %w(config/database.yml config/secrets.yml config/initializers/devise.rb)
 
@@ -67,7 +68,6 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       invoke 'puma:restart'
-      execute "export RAILS_ENV=production"
     end
   end
 
