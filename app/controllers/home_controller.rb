030704513364e892b54bd01a7fb7ba16b2cc77f6
Shaun McKinnon <totheroot@hotmail.ca>
2016-03-28 21:20:14 -0400

    asdf
    Signed-off-by: Shaun McKinnon <totheroot@hotmail.ca>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,8 @@ class HomeController < ApplicationController
   def index
     respond_to do |format|
       format.json do
-        @etsy_products = EtsyProduct.where("title ILIKE ?", "%#{params[:term]}%")params[:
-   ]     File.open "test.json", "w" do |f|
+        @etsy_products = EtsyProduct.where("title ILIKE ?", "%#{params[:term]}%")
+        File.open "test.json", "w" do |f|
           f.puts params.to_json
           f.puts @etsy_products.to_json
         end
