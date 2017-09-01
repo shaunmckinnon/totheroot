4d5d96d7d0a880fcd1b8cbd2d7c5f153737e523e
Shaun McKinnon <totheroot@hotmail.ca>
2017-09-01 11:11:55 -0400

    asdf

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,8 +2,6 @@ class ContactMailer < ApplicationMailer
   default from: Rails.application.secrets.gmail_login
 
   def contact details
-    puts "Calling ContactMailer"
-    puts details.inspect
     mail(to: Rails.application.secrets.gmail_login, subject: "Testing this shit")
   end
 end
