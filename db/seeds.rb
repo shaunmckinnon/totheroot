b84456bda02096bc8bdfa75642cc4401e928751d
Shaun McKinnon <totheroot@hotmail.ca>
2017-09-01 14:03:27 -0400

    Added title to link for shop section

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
 User.find_or_create_by(email: Rails.application.secrets.gmail_login) do |user|
-  user.encrypted_password = Rails.application.secrets.gmail_password
+  user.password = Rails.application.secrets.gmail_password
+  user.password_confirmation = Rails.application.secrets.gmail_password
 end
