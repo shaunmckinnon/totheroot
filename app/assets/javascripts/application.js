7e16a6fc1c877dcaef22fecb2391462aebcd19f5
Shaun McKinnon <shaun@firesideagency.ca>
2018-10-15 17:18:17 -0400

    Bx-is still broken

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require activestorage
-//= require turbolinks
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require jquery-ui
+//= require bxslider
+//= require social-share-button
+//= require summernote
 //= require_tree .
